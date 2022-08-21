5601ebfc88588e690c54eba7b881fcc2a5188c69
subehq <12subeedei@gmail.com>
2022-08-21 14:36:53 +0200

    Initial Commit

---
+++ PlayfulAnimations/PlayfulAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayfulAnimationsApp.swift
+//  PlayfulAnimations
+//
+//  Created by Sube Barkhasbadi on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct PlayfulAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
